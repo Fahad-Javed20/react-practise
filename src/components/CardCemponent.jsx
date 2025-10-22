e71c0f4fc70becf37d06e9b2e62aca63ac32b607
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-23 01:18:58 +0500

    Update CardCemponent.jsx

--- src/components/CardCemponent.jsx
+++ src/components/CardCemponent.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
 
-export default function CardCemponent(name,picture,salary) {
+export default function CardComponent({ name, picture, salary }) {
   return (
-    <div>
-        <div className='bg-gray-200 h-96 w-72 flex flex-col items-center gap-8'>
-<h1>{name}</h1>
-
-
-<img className='size-40 ' src={picture} alt="" />
-<p>Salary:{salary}</p>
-        </div>
+    <div className='bg-gray-200 h-96 w-72 flex flex-col items-center gap-8 rounded-2xl shadow-md p-4'>
+      <h1 className='text-2xl font-semibold mt-4'>{name}</h1>
+      <img className='size-40 rounded-full object-cover' src={picture} alt={name} />
+      <p className='text-lg font-medium'>Salary: {salary}</p>
     </div>
   )
 }
-
-
-
-
