4b957449f7f606137305b06fba3916f4ec5b5926
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-23 12:15:06 +0500

    Update CardCemponent.jsx

--- src/components/CardCemponent.jsx
+++ src/components/CardCemponent.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-export default function CardComponent({ name, picture, salary, hobbies = ["sport1", "sport2", "sport3"] }) {
+export default function CardComponent({ name, picture, salary, hobbies }) {
   return (
-    <div className='bg-gray-200 h-96 w-72 flex flex-col items-center gap-2 rounded-2xl shadow-md p-4'>
+    <div className='bg-gray-200 h-11/12 w-72 flex flex-col items-center gap-2 rounded-2xl shadow-md p-4'>
       <h1 className='text-2xl font-semibold mt-4'>{name}</h1>
       <img className='size-40 rounded-full object-cover' src={picture} alt={name} />
       <p className='text-lg font-medium'>Salary: {salary}</p>
-      <p>hobbies Are: {hobbies[0]}</p>
-      <p >hobbies Are: {hobbies[1]}</p>
-      <p >hobbies Are: {hobbies[2]}</p>
+      <h2 className='font-semibold'>Hobbies:</h2>
+        {hobbies.map((hobby, index) => (
+          <p key={index}>{hobby}</p>
+
+
+        ))}
+
+       
     </div>
   )
 }
