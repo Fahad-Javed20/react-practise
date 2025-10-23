504d692fde68bf8c41f602ede306b130c34962d9
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-23 11:38:38 +0500

    formated

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,34 @@ import './App.css'
 import CardCemponent from './components/CardCemponent'
 
 function App() {
-  
+
 
   return (
     <>
-    <div className='flex gap-3'>
+      <div className='flex gap-3'>
 
-    <CardCemponent name = "Fahad Bin Javed" picture = "https://images.unsplash.com/photo-1761069234652-9dd7fc92b845?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=500" salary = {40000000} hobbies={["snooker", "badminton", "coding"]}/>
-    <CardCemponent name = "Sir Abdul Wahab" picture = "https://images.unsplash.com/photo-1760654339407-970cede10f46?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500" salary = {40000} hobbies={["abx", "bgv", "nothing"]}/>
-    <CardCemponent name = "Aqsa Fatima" picture = "https://images.unsplash.com/photo-1760800327881-c56a3b64ff6b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500" salary = {400000} hobbies={["hokey", "squash", "playing"]}/>
-    <CardCemponent name = "Zulzar Ali" picture = "https://images.unsplash.com/photo-1760611052651-51969c2cd347?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500" salary = {400000} hobbies={["nothing", "spexial", "cooking"]}/>
-    </div>
+        <CardCemponent
+          name="Fahad Bin Javed"
+          picture="https://images.unsplash.com/photo-1761069234652-9dd7fc92b845?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=500"
+          salary={40000000}
+          hobbies={["snooker", "badminton", "coding"]}
+        />
+        <CardCemponent name="Sir Abdul Wahab"
+          picture="https://images.unsplash.com/photo-1760654339407-970cede10f46?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500"
+          salary={40000}
+          hobbies={["abx", "bgv", "nothing"]}
+        />
+        <CardCemponent name="Aqsa Fatima"
+          picture="https://images.unsplash.com/photo-1760800327881-c56a3b64ff6b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500"
+          salary={400000}
+          hobbies={["hokey", "squash", "playing"]}
+        />
+        <CardCemponent name="Zulzar Ali"
+          picture="https://images.unsplash.com/photo-1760611052651-51969c2cd347?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500"
+          salary={400000}
+          hobbies={["nothing", "spexial", "cooking"]}
+        />
+      </div>
     </>
   )
 }
--- src/components/CardCemponent.jsx
+++ src/components/CardCemponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function CardComponent({ name, picture, salary,hobbies=["sport1", "sport2", "sport3"] }) {
+export default function CardComponent({ name, picture, salary, hobbies = ["sport1", "sport2", "sport3"] }) {
   return (
     <div className='bg-gray-200 h-96 w-72 flex flex-col items-center gap-2 rounded-2xl shadow-md p-4'>
       <h1 className='text-2xl font-semibold mt-4'>{name}</h1>
